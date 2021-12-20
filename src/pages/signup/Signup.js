fae44ae18cab7929ab6fd4d85903fdb998a09964
jonytalukdar <jonytalukdar30@gmail.com>
2021-12-20 16:20:43 +0600

    added signup page

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,13 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 //styles
 import './Signup.css';
 
 const Signup = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [displayName, setDisplayName] = useState('');
+  const [thumbnail, setThumbnail] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(email, password, displayName, thumbnail);
+  };
+
   return (
-    <div>
-      <h1>signup page</h1>
-    </div>
+    <form className="auth-form" onSubmit={handleSubmit}>
+      <h2>Sign Up</h2>
+
+      <label>
+        <span>Email:</span>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </label>
+
+      <label>
+        <span>Password:</span>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </label>
+
+      <label>
+        <span>Name:</span>
+        <input
+          type="text"
+          value={displayName}
+          onChange={(e) => setDisplayName(e.target.value)}
+          required
+        />
+      </label>
+
+      <label>
+        <span>Profile Picture:</span>
+        <input type="file" required />
+      </label>
+
+      <button type="submit" className="btn">
+        Sign Up
+      </button>
+    </form>
   );
 };
 
